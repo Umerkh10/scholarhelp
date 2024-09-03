568a6ab6d34607003932472584e08f1808c922c0
umer <mumarkhan383@gmail.com>
2024-09-03 17:39:28 +0500

    form

--- src/app/(Home)/_components/BannerForm.tsx
+++ src/app/(Home)/_components/BannerForm.tsx
@@ -1,20 +1,58 @@
 "use client"
-import React from 'react'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from './FormTab';
-import { BookOpenText, Pencil, PencilRuler } from 'lucide-react';
+import React, { useState } from 'react'
+
 
 
 function BannerForm() {
+
+        // State to track which button is clicked
+        const [activeButton, setActiveButton] = useState<string | null>(null);
+      
+        // Handler to set the clicked button as active
+        const handleClick = (buttonName: string) => {
+          setActiveButton(buttonName);
+        };
   return (
-    <div  className=" py-20 bg-muted shadow-xl rounded-xl lg:w-[480px] ">
-    <div className='text-center font-bold text-xl md:text-2xl py-3 px-3  '>Get Instant Help From Our Experts</div>
-    <Tabs defaultValue='1'>
-      <TabsList className='px-3'>
-        <TabsTrigger value='1'>Writing  </TabsTrigger>
-        <TabsTrigger value='2'>Rewriting</TabsTrigger>
-        <TabsTrigger value='3'>Editing  </TabsTrigger>
-      </TabsList>
-      <TabsContent value='1'>
+    <div  className="pb-16 bg-muted shadow-xl rounded-xl lg:w-[480px] ">
+        <div className=' lg:-translate-y-5 lg:-translate-x-7 -translate-x-4  -translate-y-4'>
+        <div className='md:w-24 w-20 h-20 md:h-24 shadow-lg rounded-full bg-yellow-500 flex justify-center items-center'>
+            <div className='text-center font-medium text-zinc-100 text-xs md:text-sm'>Upto <br /> 40% <br /> Discount</div>
+        </div>
+        </div>
+    <div className='text-center font-bold text-xl md:text-2xl  px-3  '>Get Instant Help From Our Experts</div>
+    <div className='flex justify-center items-center mt-3'>
+    <button
+        className={`px-5 py-3 transition ease-in duration-200 delay-200 border-[2px]  rounded rounded-tl-lg rounded-bl-lg ${
+          activeButton === 'writing'
+            ? 'bg-blue-600 text-white'
+            : ' bg-zinc-200 text-zinc-800'
+        }`}
+        onClick={() => handleClick('writing')}
+      >
+        Writing
+      </button>
+      <button
+        className={`px-5 py-3 transition ease-in duration-200 delay-200 border-[2px] ${
+          activeButton === 'rewriting'
+            ? 'bg-blue-600 text-white'
+            : 'bg-zinc-200 text-zinc-800'
+        }`}
+        onClick={() => handleClick('rewriting')}
+      >
+        Rewriting
+      </button>
+      <button
+        className={`px-5 py-3 transition ease-in duration-200 delay-200 border-[2px]  rounded rounded-tr-lg rounded-br-lg  ${
+          activeButton === 'editing'
+            ? 'bg-blue-600 text-white'
+            : 'bg-zinc-200 text-zinc-800'
+        }`}
+        onClick={() => handleClick('editing')}
+      >
+        Editing
+      </button>
+        </div>
+
         <div className='pt-4 w-full px-3'>
             <input className='rounded-lg border-[2px] w-full py-3 px-3 outline-none' type="email" name='email' placeholder='Enter Your Email' />
         </div>
@@ -69,16 +107,7 @@ function BannerForm() {
             transition ease-in duration-200 delay-200'>Get A Free Quote</button>
         </div>
 
-      </TabsContent>
 
-      <TabsContent value='2'>
-        2222
-      </TabsContent>
-      
-      <TabsContent value='3'>
-    333333
-      </TabsContent>
-    </Tabs>
   </div>
   )
 }
