6810341eb11430ef845d4ddc5452a36eee63e2e1
umer <mumarkhan383@gmail.com>
2024-09-04 13:28:02 +0500

    loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <div className='h-screen bg-black flex justify-center items-center text-white '>Loading...</div>
+  )
+}
+
+export default Loading
