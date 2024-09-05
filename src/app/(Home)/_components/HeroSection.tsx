bf33de566663e9cc52c0b1b01a785da839f99fb7
umer <mumarkhan383@gmail.com>
2024-09-05 10:29:16 +0500

    dynamicimportform

--- src/app/(Home)/_components/HeroSection.tsx
+++ src/app/(Home)/_components/HeroSection.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Image from 'next/image';
-import BannerForm from './BannerForm';
+import dynamic from 'next/dynamic';
+const BannerForm = dynamic(() => import('./BannerForm'), { ssr: false });
+
 
 
 
