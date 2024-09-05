bb7575b0251dec533cde83bbb84b554a765e1a4a
umer <mumarkhan383@gmail.com>
2024-09-05 11:53:42 +0500

    staticpageschange

--- src/app/privacy/PrivacySection.tsx
+++ src/app/privacy/PrivacySection.tsx
@@ -17,10 +17,10 @@ function PrivacySection() {
     });
   }, [inView]);
   return (
-    <div className='mx-auto max-w-screen-xl py-10' ref={ref}>
-    <div className='pt-10 text-center font-extrabold text-3xl lg:text-5xl' data-aos='flip-down'>Data Protection and Privacy</div>
+    <div className='mx-auto max-w-screen-xl p-5' ref={ref}>
+    <div className='pt-10 text-center font-extrabold text-2xl lg:text-5xl' data-aos='flip-down'>Data Protection and Privacy</div>
 
-    <div className='py-3 text-center font-medium text-lg max-w-screen-md mx-auto'data-aos='flip-up' >At {process.env.NEXT_PUBLIC_WEBSITE_NAME}, we prioritize the privacy and security of your personal information.
+    <div className='py-3 text-center font-medium md:text-lg text-sm max-w-screen-md mx-auto'data-aos='flip-up' >At {process.env.NEXT_PUBLIC_WEBSITE_NAME}, we prioritize the privacy and security of your personal information.
          As an academic platform dedicated to enhancing your learning experience, we understand the importance of safeguarding your data. Our privacy policy outlines how we collect, use, and protect your information to ensure a secure and respectful environment for all users. We are committed to maintaining the highest standards of data protection and transparency, so you can focus on your academic pursuits with confidence.
          </div>
 </div>
--- src/app/terms/TermSection.tsx
+++ src/app/terms/TermSection.tsx
@@ -17,10 +17,10 @@ function TermSection() {
         });
       }, [inView]);
   return (
-    <div className='mx-auto max-w-screen-xl py-10' ref={ref} >
-        <div className='pt-10 text-center font-extrabold text-3xl lg:text-5xl'data-aos='flip-up'>Terms Of Service</div>
+    <div className='mx-auto max-w-screen-xl p-5' ref={ref} >
+        <div className='pt-10 text-center font-extrabold text-2xl md:text-5xl'data-aos='flip-up'>Terms Of Service</div>
 
-        <div className='py-3 text-center font-medium text-lg max-w-screen-md mx-auto'data-aos='flip-down' >By accessing or using {process.env.NEXT_PUBLIC_WEBSITE_NAME}, you agree to adhere to our terms and conditions. These terms govern your use of the platform, including all courses, materials, and support services provided. You commit to using the platform responsibly and not engaging in any activity that could compromise its functionality or the experience of other users. We reserve the right to update these terms at any time, and continued use of the platform constitutes acceptance of any changes. If you do not agree to these terms, please discontinue use of our services.</div>
+        <div className='py-3 text-center font-medium md:text-lg text-sm max-w-screen-md mx-auto'data-aos='flip-down' >By accessing or using {process.env.NEXT_PUBLIC_WEBSITE_NAME}, you agree to adhere to our terms and conditions. These terms govern your use of the platform, including all courses, materials, and support services provided. You commit to using the platform responsibly and not engaging in any activity that could compromise its functionality or the experience of other users. We reserve the right to update these terms at any time, and continued use of the platform constitutes acceptance of any changes. If you do not agree to these terms, please discontinue use of our services.</div>
     </div>
   )
 }
