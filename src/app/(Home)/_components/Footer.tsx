811325d33cdbb5487a756ef13eb80f52ce871a35
umer <mumarkhan383@gmail.com>
2024-09-05 10:01:55 +0500

    footerchanges

--- src/app/(Home)/_components/Footer.tsx
+++ src/app/(Home)/_components/Footer.tsx
@@ -11,12 +11,12 @@ const Footer = () => {
         <div  className='bg-gradient-to-b from-blue-200 via-violet-200 to-zinc-100 dark:bg-gradient-to-b dark:from-purple-900 dark:via-indigo-900 dark:to-zinc-950 grid lg:grid-cols-6 grid-cols-1 p-5 container  '>
           <div className=' col-span-2 p-4'>
             <div className='font-bold md:text-xl text-lg  '>{process.env.NEXT_PUBLIC_WEBSITE_NAME}</div>
-            <div className=' text-sm md:text-base font-medium mt-4 text-muted-foreground'>{process.env.NEXT_PUBLIC_WEBSITE_NAME} Help delivers academic writing services. Our team of qualified subject experts can help you with your challenging online classes, homework, assignments, quizzes, and exams</div>
+            <div className='font-medium mt-4 text-muted-foreground'>{process.env.NEXT_PUBLIC_WEBSITE_NAME} Help delivers academic writing services. Our team of qualified subject experts can help you with your challenging online classes, homework, assignments, quizzes, and exams</div>
 
           </div>
           <div className='p-4'>
             <div className='font-bold text-lg md:text-xl'>Explore</div>
-            <ul className='mt-3 text-muted-foreground md:text-base text-sm '>
+            <ul className='mt-3 text-muted-foreground  '>
               <li className='font-medium hover:text-zinc-800 dark:hover:text-gray-100  hover:scale-105 transition ease-in duration-150 delay-150'><Link href={"/"} >Home</Link> </li>
               <li className='font-medium  hover:text-zinc-800 dark:hover:text-gray-100 hover:scale-105 transition ease-in duration-150 delay-150'><Link href={"about"} >About Us</Link> </li>
               <li className='font-medium  hover:text-zinc-800 dark:hover:text-gray-100 hover:scale-105 transition ease-in duration-150 delay-150'><Link href={"/contact"} >Contact Us </Link> </li>            
@@ -25,7 +25,7 @@ const Footer = () => {
 
           <div className='p-4'>
             <div className='font-bold text-lg md:text-xl'>Services</div>
-            <ul className='mt-3 text-muted-foreground md:text-base text-sm '>
+            <ul className='mt-3 text-muted-foreground  '>
               <li className='font-medium  hover:text-zinc-800 dark:hover:text-gray-100 hover:scale-105 transition ease-in duration-150 delay-150'><Link href={"#"} >Online Class</Link> </li>
               <li className='font-medium  hover:text-zinc-800 dark:hover:text-gray-100 hover:scale-105 transition ease-in duration-150 delay-150'><Link href={"#"} >Online Exam</Link> </li>
               <li className='font-medium  hover:text-zinc-800 dark:hover:text-gray-100 hover:scale-105 transition ease-in duration-150 delay-150'><Link href={"#"} >Online Homework</Link> </li>
@@ -38,7 +38,7 @@ const Footer = () => {
           
           <div className='p-4'>
             <div className='font-bold text-lg md:text-xl+'>Legal Info</div>
-            <ul className='mt-3 text-muted-foreground md:text-base text-sm'>
+            <ul className='mt-3 text-muted-foreground '>
               <li className='font-medium  hover:text-zinc-800 dark:hover:text-gray-100 hover:scale-105 transition ease-in duration-150 delay-150'><Link href={"/terms"} >Terms & Conditions</Link> </li>
               <li className='font-medium  hover:text-zinc-800 dark:hover:text-gray-100 hover:scale-105 transition ease-in duration-150 delay-150'><Link href={"/privacy"} >Privacy Policy</Link> </li>
           
@@ -63,7 +63,7 @@ const Footer = () => {
           </div>
 
         </div>
-        <div className='md:text-base text-sm container py-4 bg-zinc-900 text-center text-zinc-200 text-muted-foreground'>Copyrights © 2024 All Rights Reserved by {process.env.NEXT_PUBLIC_WEBSITE_NAME}
+        <div className=' container py-4 bg-zinc-900 text-center text-zinc-200 text-muted-foreground'>Copyrights © 2024 All Rights Reserved by {process.env.NEXT_PUBLIC_WEBSITE_NAME}
         </div>
       </div>
   
