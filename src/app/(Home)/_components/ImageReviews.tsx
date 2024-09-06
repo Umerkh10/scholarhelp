5cb42f98ac33f75d9924eccc54b7653b1f0d3bac
umer <mumarkhan383@gmail.com>
2024-09-06 14:51:39 +0500

    headingchanges

--- src/app/(Home)/_components/ImageReviews.tsx
+++ src/app/(Home)/_components/ImageReviews.tsx
@@ -7,8 +7,8 @@ const ImageReviews = () => {
 
   return (
     <div className='mx-auto max-w-screen-xl mt-10 '  data-aos="zoom-in" >
-        <div className='text-xl text-center md:text-4xl dark:text-zinc-100 text-violet-950 font-extrabold'>Demonstrating Excellence: </div>
-        <div className='text-2xl text-center md:text-5xl dark:text-zinc-100 text-violet-950 font-extrabold'> Students Earned A-Grades! </div>
+        <div className='text-lg text-center md:text-4xl dark:text-zinc-100 text-violet-950 font-extrabold'>Demonstrating Excellence: </div>
+        <div className='text-xl text-center md:text-5xl dark:text-zinc-100 text-violet-950 font-extrabold'> Students Earned A-Grades! </div>
         <div className='grid lg:grid-cols-2 grid-cols-1 gap-4 p-5' data-aos="fade-up">
             <div className='mt-4'>
                 <Image src="/imgs/ss-results.jpg" width={800} height={400} alt='ss-result' ></Image>
--- src/app/(Home)/_components/Rating.tsx
+++ src/app/(Home)/_components/Rating.tsx
@@ -8,7 +8,7 @@ const Rating = () => {
         <div className='pt-10'>
             <div className='text-2xl md:text-5xl font-extrabold text-center dark:text-zinc-100 text-violet-950'>Our Reviews</div>
             <div className='grid lg:grid-cols-2 grid-cols-1 mx-auto max-w-screen-xl'>
-                <div className=' p-7'>
+                <div data-aos="fade-up" className=' p-7'>
                     <div className='flex flex-col md:flex-row items-center space-y-3 md:space-y-0 md:space-x-4'>
                         <Card image='/imgs/confidential-folder.png' title='100% Confidentiality' isStar={false} />
                         <Card image='/imgs/spam.png' title='No Spam' isStar={false} />
@@ -20,13 +20,13 @@ const Rating = () => {
                     </div>
                 </div>
 
-                <div className=' p-7'>
+                <div data-aos="fade-up" className=' p-7'>
                     <div className='flex flex-col md:flex-row items-center space-y-3 md:space-y-0 md:space-x-4'>
                     <Card image='/imgs/google.png' title='Google Reviews' isStar={true} />
                     <Card image='/imgs/trust-pilot.png' title='Trust Pilot' isStar={true} />
                     </div>
 
-                    <div className='flex flex-col md:flex-row items-center mt-4 space-y-3 md:space-y-0 md:space-x-4'>
+                    <div  className='flex flex-col md:flex-row items-center mt-4 space-y-3 md:space-y-0 md:space-x-4'>
                     <Card image='/imgs/site-jabber.png' title='Site Jabber' isStar={true} />
                     <Card image='/imgs/reviews.io.png' title='Reviews.io' isStar={true} />
                     </div>
@@ -42,7 +42,7 @@ export default Rating
 
 const Card = ({image, title, isStar }: { image:string, title: string, isStar: boolean }) => {
     return (
-        <div  data-aos="fade-up" className="w-full sm:w-[380px] rounded-3xl bg-gradient-to-b p-1 from-blue-800 to-purple-800">
+        <div   className="w-full sm:w-[380px] rounded-3xl bg-gradient-to-b p-1 from-blue-800 to-purple-800">
             <div className="rounded-[calc(1.5rem-5px)] bg-white p-3 dark:bg-gray-900">
                 <div className="flex items-center gap-4">
                     <Image className="rounded-full" src={image} width={50} height={50} alt="rating" />
