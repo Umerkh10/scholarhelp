012881cf16c2a28d55ac09ae154f9eceab7e7c63
umer <mumarkhan383@gmail.com>
2024-09-10 10:23:27 +0500

    animatechanges

--- src/app/(Home)/_components/HeroSection.tsx
+++ src/app/(Home)/_components/HeroSection.tsx
@@ -9,7 +9,7 @@ export const HeroSection = () => {
   return (
 
     <div className='m-h-[500px] border-b-2 border-indigo-200 -z-10 h-full w-full bg-gradient-to-t from-sky-200 via-purple-200 to-zinc-100 dark:-z-10 dark:m-h-[500px] dark:w-full dark:[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]'>
-      <div className='grid lg:grid-cols-2 grid-cols-1  mx-auto max-w-screen-xl py-10 ' >
+      <div className='grid lg:grid-cols-2 grid-cols-1 mx-auto max-w-screen-xl py-10 '>
         <div className='py-20 px-5 md:pt-40'>
           <h1 className='text-center lg:text-left dark:text-zinc-100 text-zinc-900 text-xl sm:text-4xl font-extrabold '>Take My Online Class for me in USA and Ace My Exam</h1>
           <h2 className='text-center py-1 lg:text-left dark:text-zinc-100 text-zinc-900 text-xl sm:text-2xl font-bold'>Taking My Affordable Classes Online Guarantee You the Academic Distinction</h2>
--- src/app/(Home)/_components/LongContent.tsx
+++ src/app/(Home)/_components/LongContent.tsx
@@ -48,7 +48,7 @@ export default LongContent
 function DynamicContent2() {
   return (
     <>
-    <div data-aos='flip-down' className='mx-auto max-w-screen-xl mt-5'>
+    <div className='mx-auto max-w-screen-xl mt-5'>
     <div className='p-5'>
         <h2 className='font-extrabold text-xl md:text-4xl dark:text-zinc-100 text-indigo-950 mt-3'>
             Why do students often say do my online class? </h2>
@@ -107,7 +107,7 @@ function DynamicContent1() {
   return (
     <>
  
-    <div data-aos='flip-down'> 
+    <div > 
         <h2 className='font-extrabold text-xl md:text-4xl dark:text-zinc-100 text-indigo-950 mt-3'>The Evaluation of Online Classes System Over Education System </h2>
     <p className='font-medium text-muted-foreground md:text-base text-sm pt-4 '>Today’s world is moving towards virtual opportunities. With the advent of AI, technology, and better internet connections worldwide, taking online classes has become common. You can enrol in an online class anywhere. If you write on Google ‘do my exam online’, you will find a number of services. You only need a laptop and an internet connection to access the classes. Moreover, we now witness virtual universities offering online courses instead of virtual classes. Lastly, this evaluation signifies the value of online classes.  </p>
 
