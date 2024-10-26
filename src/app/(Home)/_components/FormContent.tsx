f2e2001536badc4b690082445c1b3d9bd105d94f
umer <mumarkhan383@gmail.com>
2024-10-26 16:27:49 +0500

    formcontentchanges

--- src/app/(Home)/_components/FormContent.tsx
+++ src/app/(Home)/_components/FormContent.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { CtaButtons } from './HeroSection';
 import Image from 'next/image';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
 
 
 function FormContent() {
@@ -12,20 +13,29 @@ function FormContent() {
                 <div className=''>
 
                     <div data-aos="flip-down" className="md:pt-6 pt-8 "  >
-                        <h2 className="cursor-pointer py-2 rounded rounded-tr-[20px] rounded-tl-[20px]  md:text-xl font-bold flex justify-between group  ">
-                            Explore Your Unique Learning Dimension through Our Exclusive Online Tutors  </h2>
-
+                    <Accordion type="single" defaultValue='item-1' collapsible>
+                    <AccordionItem value="item-1">
+                    <AccordionTrigger>
+                        <h2 className="cursor-pointer text-left py-2 rounded rounded-tr-[20px] rounded-tl-[20px]  md:text-xl font-bold flex justify-between group  ">Explore Your Unique Learning Dimension through Our Exclusive Online Tutors  </h2>
+                    </AccordionTrigger>
+                    <AccordionContent>
                         <p className=" py-4 rounded  rounded-br-[10px] rounded-bl-[10px] text-sm md:text-base  ">
                             takingmyclassesonline.com is the leading online platform for you to hire an online class instructor. Somehow, you might be bored of taking online classes and wondering about ‘need help with my online class’. So, then, we are here to help you out. We are on a mission to help students with their academics so they can easily continue their learning journey.
                         </p>
-                        <h2 className="cursor-pointer py-2 rounded rounded-tr-[20px] rounded-tl-[20px]  md:text-xl font-bold flex justify-between group  ">
-                            Can I Pay Someone to Take My Classes Online</h2>
+                    </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value='item-2'>
+                    <AccordionTrigger>
+                        <h2 className="cursor-pointer py-2 rounded rounded-tr-[20px] rounded-tl-[20px]  md:text-xl font-bold flex justify-between group  "> Can I Pay Someone to Take My Classes Online</h2>
+                    </AccordionTrigger>
+                    <AccordionContent>
                         <p className=" py-4 rounded  rounded-br-[10px] rounded-bl-[10px] text-sm md:text-base  ">
                             If you are searching for  Pay Someone to Take My Classes Online, so you are at the right place to get online class services to score high in your exam. Literally, concerning our expert makes you eligible to reach the distinctions. Moreover, our experts are highly qualified, skilled, and well aware of modern teaching techniques. Also, their experience in this field innovates their teaching methodology as well as they are very committed and sincere in their prophetic profession. So, it is your decision whether you want to achieve your success or not. In reality, you are one step away from filling out the order form and taking your exam.
                             Pay someone to take my class? is not a simple question as it requires extensive research to find the best online service for this job. By hiring our online instructor, you will lead your academic journey and get the distinction.
                         </p>
-
-
+                        </AccordionContent>
+                        </AccordionItem>
+                        </Accordion>
                     </div>
 
                     <CtaButtons/>
@@ -33,7 +43,7 @@ function FormContent() {
 
                 <div data-aos="zoom-in-down" className=''>
                     <div className='flex items-center justify-center mt-5'>
-                        <Image src={'/imgs/form-content-img.png'} width={800} height={500} alt='content-img'></Image>
+                        <Image src={'/imgs/form-content-img.webp'} width={800} height={800} alt='content-img'></Image>
                     </div>
                 </div>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ const config = {
   prefix: "",
   theme: {
   	container: {
-  		center: true,
+  		center: 'true',
   		padding: '2rem',
   		screens: {
   			'2xl': '1400px'
@@ -74,14 +74,11 @@ const config = {
   				to: {
   					height: '0'
   				}
-  			},
-  	
-  
+  			}
   		},
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out',
-  			
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
