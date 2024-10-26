c7f6fbde9961a68d7821146126ba2e5f81f43d10
umer <mumarkhan383@gmail.com>
2024-10-26 15:49:47 +0500

    herosectionchanges

--- src/app/(Home)/_components/HeroSection.tsx
+++ src/app/(Home)/_components/HeroSection.tsx
@@ -12,22 +12,22 @@ export const HeroSection = () => {
     <div className='m-h-[500px] border-b-2 border-indigo-200 -z-10 h-full w-full bg-gradient-to-t from-sky-200 via-purple-200 to-zinc-100 dark:-z-10 dark:m-h-[500px] dark:w-full dark:[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]'>
       <div className='grid lg:grid-cols-2 grid-cols-1 mx-auto max-w-screen-xl py-10 '>
         <div className='py-20 px-5 md:pt-36'>
-          <h1 className=' text-center py-2 lg:text-left dark:text-zinc-100 text-zinc-900 text-xl sm:text-4xl font-extrabold '>Take My Online Class for me in USA and Ace My Exam</h1>
-          <h2 className='text-center mt-2 lg:text-left dark:text-zinc-100 text-zinc-900 text-lg sm:text-2xl font-medium'>Taking My Affordable Classes Online Guarantee You the Academic Distinction</h2>
+          <h1 className=' text-center py-2 lg:text-left dark:text-zinc-100 text-zinc-900 text-lg sm:text-4xl font-extrabold '>Take My Online Class for me in USA and Ace My Exam</h1>
+          <h2 className='text-center mt-2 lg:text-left dark:text-zinc-100 text-zinc-900  sm:text-2xl font-medium'>Taking My Affordable Classes Online Guarantee You the Academic Distinction</h2>
           <div className='flex flex-col lg:flex-row items-center xl:w-[50vw] gap-4 sm:mt-2 mt-6'>
             <div className='flex items-center gap-1'>
             <Image src={'/imgs/instructor.png'} width={64} height={64} alt='instructor'></Image>
-          <h3 className='text-center dark:text-zinc-100 text-zinc-900 md:text-base font-medium'>950+ PhD Instructor</h3>
+          <h3 className='text-center dark:text-zinc-100 text-zinc-900 text-sm md:text-base font-medium'>950+ PhD Instructor</h3>
           <div className='hidden lg:block w-[2px] h-20 bg-muted-foreground '></div>
           </div>
             <div className='flex items-center gap-1'>
             <Image src={'/imgs/record.png'} width={64} height={64} alt='record'></Image>
-          <h3 className='text-center dark:text-zinc-100 text-zinc-900 md:text-base font-medium'>Proven Track Record</h3>
+          <h3 className='text-center dark:text-zinc-100 text-zinc-900 text-sm md:text-base font-medium'>Proven Track Record</h3>
           <div className='hidden lg:block w-[2px] h-20 bg-muted-foreground '></div>
           </div>
             <div className='flex items-center gap-1'>
             <Image src={'/imgs/affordable.png'} width={64} height={64} alt='phd'></Image>
-          <h3 className='ml-1 dark:text-zinc-100 text-zinc-900 md:text-base font-medium'>Affordable Packages</h3>
+          <h3 className='ml-1 dark:text-zinc-100 text-zinc-900 text-sm md:text-base font-medium'>Affordable Packages</h3>
           <div className='hidden lg:block w-[2px] h-16 bg-muted-foreground/0 '></div>
           </div>
           </div>
